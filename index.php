8fd20e8222cbf07ad410de119a10533e1d26670b
FemiGithub <femiawojana@yahoo.com>
2018-04-11 04:45:13 +0100

    Add existing file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="EN-US">
+<head>
+  <title>Challenge 1</title>
+  <meta charset="utf-8">
+  <link href="./index.css" rel="stylesheet">
+</head>
+<body>
+  <?php
+   date_default_timezone_set("Africa/Lagos");
+   echo "<figure><img src=\"./htmlimage/hotelNG.png\" width=\"244px\" height=\"111px\"></figure>";
+   echo "<p class=\"text1\">CHALLENGE 1 COMPLETED </p>";
+   echo"<p class=\"text2\">".date("h.i");"</p>";
+  ?>
+</body>
+</html>
